049b467c2b070d27cfe668fad1f8d57da4886853
matepa12 <matepa12@gmail.com>
2020-12-10 15:54:29 +0100

    Learning Intellij and GitHub interaction.

---
+++ main.py
@@ -0,0 +1,19 @@
+import engine
+import db
+import os
+
+if __name__ == '__main__':
+
+    db_path = "bicycle_database.sqlite"
+
+    if os.path.exists(db_path) is False:
+        init_db = db.DBCreation(db_path)
+        init_db.create_tables()
+        init_db.populate_tables()
+    else:
+        init_db = db.DBCreation(db_path)
+
+    main_window = engine.Engine()
+    main_window.mainloop()
+
+    init_db.close()
